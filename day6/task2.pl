7f7cfe917d56c1a3dde65d570965e1e30b26b690
Andy Chase <anchase@ip-10-137-144-26.eu-west-2.compute.internal>
2023-12-06 17:47:15 +0000

    day6 task2

---
+++ day6/task2.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+use Data::Dumper;
+use strict;
+my $answer = 1;
+my $times = <DATA>;
+my $distances = <DATA>;
+chomp $times;
+chomp $distances;
+$times =~ s/\D//g;
+$distances =~ s/\D//g;
+my %records = ( $times => $distances );
+
+for my $time (sort {$a <=> $b} keys %records) {
+    my $count = 0;
+    my @range = reverse (1 .. int($time/2));
+    my $start = 1;
+    while (my $speed = $range[midPoint(\@range, \$start)]) {
+#   for my $speed (@range) {
+        my $wins = race($time, $speed);
+        print ":${wins}";
+        # if it wins, move forward half again.
+        if ($wins) {
+#           my $remain = scalar @range - $start;
+#           $start += int($remain/2);
+            next;
+        }
+        # if it loses, everything after that record loses. delete them
+        $#range = $start;
+        do {
+            $#range--;
+            $answer = 2* @range . "\n";
+        } until (race( $time, $range[-1]));
+        
+    }
+}
+$answer-- if ($times =~ /[24680]$/);
+print "\n${answer}\n";
+
+sub midPoint {
+    my ($arrayRef, $startRef) = @_;
+    my $size = scalar @{$arrayRef};
+    my $start = ${$startRef};
+    my $pos = int(($size-$start)/2) + $start;
+    ${$startRef} = $pos;
+    print "\nSize:Pos:Started:now:Wins" . join(':', scalar @{$arrayRef}, $pos, "${start}=>${$startRef}");
+    return $pos;
+}
+
+sub race {
+    my ($time, $speed) = @_;
+    my $ttl = $time - $speed;
+    my $travelDistance = $ttl * $speed;
+    my $wins = $travelDistance if ($travelDistance > $records{$time});
+    printf("racetime: %d, button: %d, speed: %d, timeLeft: %d distance: %d record: %d (%s)\n"
+    ,   $time
+    ,   $speed
+    ,   $speed
+    ,   $ttl
+    ,   $travelDistance
+    ,   $records{$time}
+    ,   $wins
+    ) if (0);
+    return $wins;
+}
+__DATA__
+Time:        47     70     75     66
+Distance:   282   1079   1147   1062
